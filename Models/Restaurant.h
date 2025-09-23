7a263694a7f7696eaf441f87b57b6a4490a5556d
mukeshlilawat1 <mukeshkumarlilawat1@gmail.com>
2025-09-23 12:51:55 +0530

    Implement Cart and Restaurant classes with basic functionality

--- Models/Restaurant.h
+++ Models/Restaurant.h
@@ -14,4 +14,45 @@ class Restaurant {
     string name;
     string location;
     vector<MenuItem> menu;
-}
+
+    public :
+    Restaurant(const string& name, const string& location) {
+        this->name = name;
+        this->location = location;
+        this->restaurantId = ++nextRestaurantId;
+    }
+
+    ~Restaurant() {
+        // just for clarity or debug
+        cout << "Destroying Restaurant : " << name << " , and clearing its menu. " << endl;
+        menu.clear();
+    }
+
+      //Getters and setters
+    string getName() const {
+        return name;
+    }
+
+    void setName(const string &n) {
+        name = n;
+    }
+
+    string getLocation() const {
+        return location;
+    }
+
+    void setLocation(const string &loc) {
+        location = loc;
+    }
+
+    void addMenuItem(const MenuItem &item) {
+        menu.push_back(item);
+    }
+
+    const vector<MenuItem>& getMenu() const {
+        return menu;
+    }
+};
+
+int Restaurant::nextRestaurantId = 0;
+#endif
