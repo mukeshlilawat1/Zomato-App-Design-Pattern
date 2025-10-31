ac0c190f0fd1fb2729d6de75598d709191dd2874
mukeshlilawat <mukeshkumarlilawat1@gmail.com>
2025-10-31 22:35:29 +0530

    NowOrderFactory.h file are completed

---
+++ Factories/ScheduledOrderFactory.h
@@ -0,0 +1,11 @@
+#ifndef SCHEDULED_ORDER_FACTORY_H
+#define SCHEDULED_ORDER_FACTORY_H
+
+#include "OrderFactory.h"
+#include "../Models/DeliveryOrder.h"
+#include "../Models/PickupOrder.h"
+#include "../Utils/TimeUtiles.h"
+
+class ScheduledOrderFactory : public OrderFactory {
+    private:
+}
